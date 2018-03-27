9be366a34c7c3a2f419eae2cdb1dcb471c604bea
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-03-27 12:09:40 +0200

    speek syntesis

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Speech.Synthesis;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(DateTime.Now);
+            SpeechSynthesizer mowa = new SpeechSynthesizer();
+            mowa.Speak("what the fuck is going on?");
         }
     }
 }
