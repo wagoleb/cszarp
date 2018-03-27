2d3de9a72fa11472e97e73d6b4bbaa4dc2048c70
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-03-27 15:15:10 +0200

    testy

---
+++ ConsoleApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            bool prawda = true;
+
+            Assert.IsTrue(prawda);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            bool prawda = false;
+
+            Assert.IsTrue(prawda);
+        }
+    }
+
+
+}
