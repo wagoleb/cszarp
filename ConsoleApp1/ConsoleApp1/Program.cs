5e4597b910fdd2fad482864e825c1b8490691761
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-04-04 10:57:48 +0200

    completed

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,19 +1,35 @@
 ﻿using System;
-using System.Speech.Synthesis;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ConsoleApp1
 {
+    public class Kata
+    {
+        public static string BreakCamelCase(string str)
+        {
+            string result = "";
+            int start = 0, end = str.Length;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i].ToString().Any(char.IsUpper) == true)
+                {
+                    result += " " + str[i];
+                }
+                else
+                {
+                    result += str[i];
+                }
+            }
 
+            return result;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-
-
+            Console.WriteLine(Kata.BreakCamelCase("camelCasingTest"));
         }
     }
 }
