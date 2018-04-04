149f4a90ad0353fe673320acf805144784288b2a
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-04-04 10:03:56 +0200

    remove some shit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,46 +7,12 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Nowa
-    {
-        private string _myprops;
-        public string MyProperty
-        {
-            get
-            {
-                Console.WriteLine("getter");
-                if (!String.IsNullOrEmpty(_myprops))
-                {
-                    Console.WriteLine("Nie pusty");
-                    return _myprops;
-                }
-                else
-                {
-                    Console.WriteLine("PUSTY!");
-                    return "";
-                }
-            }
-            set
-            {
-                Console.WriteLine("setter");
-                if (!String.IsNullOrEmpty(value))
-                {
-                    Console.WriteLine("OK");
-                    _myprops = value;
-                }
-                else
-                {
-                    Console.WriteLine("Pusty");
-                }
-            }
-        }
-    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Nowa obiekt1 = new Nowa();
-            string nowy = obiekt1.MyProperty;
+
 
         }
     }
