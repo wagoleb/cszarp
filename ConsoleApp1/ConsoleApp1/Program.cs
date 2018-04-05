388ecb0cafd8ff561c5b96e51e5d548f8ad9fe2f
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-04-05 09:17:24 +0200

    moving zeros

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,17 +1,39 @@
 ﻿using System;
-using System.Speech.Synthesis;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-   
+    public class Kata
+    {
+        public static int[] MoveZeroes(int[] arr)
+        {
+            List<int> start = new List<int>(), zerowa = new List<int>();
+            foreach (var item in arr)
+            {
+                if (item != 0)
+                {
+                    start.Add(item);
+                }
+                else
+                {
+                    zerowa.Add(item);
+                }
+            }
+            start.AddRange(zerowa);
+            arr = start.ToArray();
+            return arr;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            foreach (var item in Kata.MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 }))
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
