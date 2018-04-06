e3f37fa52d8ef3e707b7b428d03453d1eb92160c
bonowg <bonowg@gmail.com>
2018-04-06 14:41:41 +0200

    cleaning

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,29 +4,10 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
-    public class ProdFib
-    {
-        public static int Fibonacci(int prod)
-        {
-            if (prod == 1)
-            {
-                return 0;
-            }
-            else if ( prod >=2 && prod <= 3)
-            {
-                return 1;
-            }
-            else
-            {
-                return Fibonacci(prod - 1) + Fibonacci(prod - 2);
-            }
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ProdFib.Fibonacci(10));
         }
     }
 }
