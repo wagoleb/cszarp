aff8da0fb1448960a38e1f91c12fd88b825cf212
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-04-11 15:08:04 +0200

    wyjatki

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,11 +4,20 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
- 
+
     class Program
     {
         public static void Main(string[] args)
         {
+            int a = 5, b = 0;
+            try
+            {
+                int c = a / b;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("dzielenie przez zero");
+            }
         }
     }
 }
