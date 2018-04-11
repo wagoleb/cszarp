b82ccda1f6c06d5428c4d28297a92506bc15e6b6
wagolebx <wojciechx.a.golebiewski@intel.com>
2018-04-11 10:21:05 +0200

    clean

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,20 +6,5 @@ namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            ArrayList nowaLista = new ArrayList();
-           nowaLista.Add(5);
-            nowaLista.Add(5);
-            nowaLista.Add("wojtek");
-            nowaLista.Add("g");
-
-            foreach (var item in nowaLista)
-	{
-                Console.WriteLine(item);
-	}
-            Console.WriteLine(nowaLista[2]);
-            Console.ReadKey();
-        }
     }
 }
