d02335e9c2027964cebb0b947668b39b0fb04915
bonowg <bonowg@gmail.com>
2018-04-11 21:34:54 +0200

    codewarsex1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,13 +4,31 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
-
+    public static class TimeFormat
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            TimeSpan t = TimeSpan.FromSeconds( seconds );
+            int tHours;
+            int hours = seconds / 3600;
+            if (hours > 0)
+                {
+                    tHours = hours;
+                }
+            else
+                {
+                    tHours = t.Hours;
+            }
+            return String.Format("{0:D2}:{1:D2}:{2:D2}", tHours, t.Minutes, t.Seconds);
+        }
+    }
     class Program
     {
         public static void Main(string[] args)
         {
-  
-            }
+            Console.WriteLine(TimeFormat.GetReadableTime(359999));
+
+            Console.ReadKey();
         }
     }
 }
