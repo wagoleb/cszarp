4260de4940509061e370d41886f1e9de2130ff92
bonowg <bonowg@gmail.com>
2018-04-11 21:07:05 +0200

    czyszczenie

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -9,27 +9,7 @@ namespace ConsoleApp1
     {
         public static void Main(string[] args)
         {
-            int a = 5, b = 0;
-            try
-            {
-                int c = a / b;
-            }
-            catch (DivideByZeroException e)
-            {
-                Console.WriteLine("dzielenie przez zero");
-                Console.WriteLine(e.Message);
-
-                int? x;
-                x = null;
-                x= 56;
-                Nullable<int> y;
-                y = null;
-
-                int z;
-
-                z = x ?? 0;
-                Console.WriteLine(z);
-                Console.ReadKey();
+  
             }
         }
     }
