7b0567b646f725918300c1e68b6480040d5b4743
bonowg <bonowg@gmail.com>
2018-04-14 20:24:59 +0200

    code wars 2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,20 +5,60 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
-    public class Kata
+    public class NumberTranslation
     {
-        public static string FirstNonRepeatingLetter(string s)
+
+
+        public static string Number2Words(int number)
         {
-            for ( int i = 0; i < s.Length; i++)
+            if (number == 0)
+                return "zero";
+
+            if (number < 0)
+                return "minus " + Number2Words(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 1000000) > 0)
             {
-                string getLower = s.ToLower();
-                if (getLower.Count(x => x == getLower[i]) == 1)
+                words += Number2Words(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += Number2Words(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += Number2Words(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += " ";
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                    words += unitsMap[number];
+                else
                 {
-                    return Convert.ToString(s[i]);
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
                 }
             }
-            return "";
+
+            return words.TrimEnd();
         }
+
+
     }
 
     class Program
@@ -26,7 +66,7 @@ namespace ConsoleApp1
 
         public static void Main(string[] args)
         {
-            Console.WriteLine(Kata.FirstNonRepeatingLetter("wwoojjTeekk"));
+            Console.WriteLine(NumberTranslation.Number2Words(800));
         }
     }
 }
