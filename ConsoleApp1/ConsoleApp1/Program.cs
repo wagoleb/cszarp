b07118e50a337f22d43896446d5112939a35cef0
bonowg <bonowg@gmail.com>
2018-04-14 20:13:22 +0200

    codewars challenge 1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,14 +5,28 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
-
+    public class Kata
+    {
+        public static string FirstNonRepeatingLetter(string s)
+        {
+            for ( int i = 0; i < s.Length; i++)
+            {
+                string getLower = s.ToLower();
+                if (getLower.Count(x => x == getLower[i]) == 1)
+                {
+                    return Convert.ToString(s[i]);
+                }
+            }
+            return "";
+        }
+    }
 
     class Program
     {
 
         public static void Main(string[] args)
         {
-
+            Console.WriteLine(Kata.FirstNonRepeatingLetter("wwoojjTeekk"));
         }
     }
 }
