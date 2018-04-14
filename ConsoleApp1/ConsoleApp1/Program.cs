5b520b6bbf69b1f325107e9b0734c5eca3008ebd
bonowg <bonowg@gmail.com>
2018-04-14 19:58:30 +0200

    cleeaning

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,26 +5,14 @@ using System.Linq;
 
 namespace ConsoleApp1
 {
-    static class StringExtend
-    {
-        public static int CountChar(this string word, char n)
-        {
-            return word.Count(x => x == n);
-        }
-    }
+
 
     class Program
     {
 
         public static void Main(string[] args)
         {
-            string wyrazenie = "wwwoooojjjteeekkkkkkkkkkkkkkoo";
-            foreach (var item in wyrazenie)
-            {
-                Console.WriteLine("znak {0} wystepuje {1} razy", item, wyrazenie.CountChar(item));
-            }
 
-            Console.ReadKey();
         }
     }
 }
