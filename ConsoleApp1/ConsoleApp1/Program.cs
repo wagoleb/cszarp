f9abc219b797a5c8dd1f3dc243faef657cd81d27
bonowg <bonowg@gmail.com>
2018-04-17 21:32:38 +0200

    cxzv

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,72 +1,32 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 
 
 namespace ConsoleApp1
 {
-    public class NumberTranslation
+    public static class Kata
     {
-
-
-        public static string Number2Words(int number)
+        public static string sumStrings(string a, string b)
         {
-            if (number == 0)
-                return "zero";
-
-            if (number < 0)
-                return "minus " + Number2Words(Math.Abs(number));
-
-            string words = "";
-
-            if ((number / 1000000) > 0)
+            int maxLength = (a.Length >= b.Length) ? a.Length : b.Length;
+            for ( int i = maxLength-1; i > -1; i--)
             {
-                words += Number2Words(number / 1000000) + " million ";
-                number %= 1000000;
+                Console.WriteLine("{0} - {1}", a[i], b[i]);
             }
-
-            if ((number / 1000) > 0)
-            {
-                words += Number2Words(number / 1000) + " thousand ";
-                number %= 1000;
-            }
-
-            if ((number / 100) > 0)
-            {
-                words += Number2Words(number / 100) + " hundred ";
-                number %= 100;
-            }
-
-            if (number > 0)
-            {
-                if (words != "")
-                    words += " ";
-
-                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-
-                if (number < 20)
-                    words += unitsMap[number];
-                else
-                {
-                    words += tensMap[number / 10];
-                    if ((number % 10) > 0)
-                        words += "-" + unitsMap[number % 10];
-                }
-            }
-
-            return words.TrimEnd();
+            return Convert.ToString(maxLength);
         }
-
-
     }
-
     class Program
     {
 
         public static void Main(string[] args)
         {
-            Console.WriteLine(NumberTranslation.Number2Words(800));
+            // Kata.sumStrings("1234", "456");
+            string slowo = "wojtek";
+            IEnumerable<char> nowe = slowo.Reverse();
+            Console.WriteLine((string)nowe);
         }
     }
 }
